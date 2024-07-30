3426ef522983da4e1a29f4697c823a0dd906e201
Savouert Pheap <savouert.pheap@camsolution.biz>
2024-07-30 17:33:03 +0700

    update

---
+++ Assigment-7/src/view/admin/Dashbord.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard-page">
+    <div class="container">
+      <nav class="menu">
+        <h2>Admin</h2>
+        <ul>
+          <li><a href="#">Users</a></li>
+          <li><a href="#">Products</a></li>
+        </ul>
+      </nav>
+      <main class="content">
+        <h1>Users Management</h1>
+        <p>user</p>
+      </main>
+    </div>
+    <ul>
+      <!-- <li v-for="(el, indx) in products" :key="indx">{{ el.id }} - {{ el.title }}</li> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "DashboardPage"
+}
+</script>
---
+++ Assigment-7/src/view/admin/login.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Login from '../../components/LoginForm.vue'
+</script>
+
+<template>
+    <div>
+        <Login/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"LoginPage"
+}
+</script>
